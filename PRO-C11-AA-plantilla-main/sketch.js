0847de88bedd4bcfa19932fb8976f96fb3ba6ec9
arrowo <99696049+arrowo@users.noreply.github.com>
2022-02-27 14:26:54 -0600

    Add files via upload

---
+++ PRO-C11-AA-plantilla-main/sketch.js
@@ -0,0 +1,19 @@
+var weight = [35,38,42,45,43,34,36,41,48,32];
+var suma =0;
+function setup() {
+  createCanvas(400,400);
+for(var conteo=0;conteo=weight.length;conteo ++){
+suma = suma + weight [conteo]
+  
+}
+var promedio = suma / weight.length;
+console.log(promedio)
+}
+
+function draw() 
+{
+  background(30);
+}
+
+ 
+
